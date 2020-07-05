dfb49eab51b4437c824a0bbec1c0418fe7917853
roman.eguen <roman.eguen@nter.es>
2020-07-05 06:03:19 +0200

    Dependencies&BasicConnections

---
+++ src/main/java/com/RomanEguenRuiz/LDAPauth/LdaPauthApplication.java
@@ -0,0 +1,13 @@
+package com.RomanEguenRuiz.LDAPauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdaPauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdaPauthApplication.class, args);
+	}
+
+}
